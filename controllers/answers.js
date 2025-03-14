71f8775c7a89296e56bd663584679016956c6058
psycho <aali571110@gmail.com>
2025-03-14 10:22:51 +0200

    get asnwers

--- controllers/answers.js
+++ controllers/answers.js
@@ -1,3 +1,4 @@
+const answer = require("../models/answer")
 const Answer=require("../models/answer")
 const Question=require("../models/question")
 const add=(req,res,next)=>{
@@ -17,6 +18,13 @@ const add=(req,res,next)=>{
             questionId
         })
         return answer.save()
+        .then((answer)=>{
+            return{answer,question}
+        })
+    })
+    .then(({answer,question})=>{
+        question.answer.push(answer._id)
+        return question.save()
     })
     .then(()=>{
         res.status(200).json({
@@ -27,4 +35,28 @@ const add=(req,res,next)=>{
         next(err)
     })
 }
+const getAll=(req,res,next)=>{
+    const currentPage= req.query.page
+    const perPage=5
+    let totalAnswers
+    Answer.countDocuments()
+    .then(count=>{
+        totalAnswers=count
+        return Answer.find()
+        .populate({path:"userId",select:"name"})
+        .skip((currentPage-1)*perPage)
+        .limit(perPage)
+    })
+    .then(answers=>{
+        res.status(200).json({
+            message:"Fetched answers successfully!",
+            answers,
+            totalAnswers
+        })
+    })
+    .catch(err=>{
+        next(err)
+    })
+}
 exports.add=add
+exports.getAll=getAll
