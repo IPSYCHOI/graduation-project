7e66897c8df427d0c8a3473c498c0fbcc83875a2
psycho <aali571110@gmail.com>
2025-03-17 09:24:32 +0200

    add laravel api

--- routes/questions.js
+++ routes/questions.js
@@ -2,9 +2,11 @@ const express = require("express")
 
 const router=express.Router()
 
+const {api}=require("../middlewares/fetchApi")
+
 const questionController=require("../controllers/question")
 
-router.post("/add",questionController.add)
+router.post("/add",api,questionController.add)
 router.get("/all",questionController.getAll)
 
 module.exports=router
