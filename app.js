41a863c9aec79905e936d6567f10996a9366457c
psycho <aali571110@gmail.com>
2025-03-24 07:51:00 +0200

    ahh tany

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.use("/questions",questionRouters)
 
 dbconnect()
 .then(()=>{
-    app.listen(process.env.PORT,() => console.log("🚀 Server running on port 80 "))
+    app.listen(process.env.PORT,() => console.log("🚀 Server running on port 80 ahhh "))
 })
 app.use(errorHandler)
