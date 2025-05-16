3265e2e835ae087cef0d9e410a2d42568fafe8a9
psycho <aali571110@gmail.com>
2025-05-16 04:14:24 +0300

    typing sockets

--- socket/handelers/unSeen.js
+++ socket/handelers/unSeen.js
@@ -2,8 +2,6 @@ const Message=require("../../models/message")
 exports.unSeen=async(socket)=>{
     const chatId = socket.chatId
     const userId=socket.apiData.data.id
-    const name = socket.apiData.data.name
-    const avatar = socket.apiData.data.avatar
     if(!socket.chatId){
         return socket.emit("un-seen-error",{
             message:"no chatId Register first"
