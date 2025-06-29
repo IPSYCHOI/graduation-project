e268e538efea1eeaa9fa8352c612a860f7ba1288
psycho <aali571110@gmail.com>
2025-06-29 20:42:25 +0300

    del

--- notifications/firebase/firebaseService.js
+++ notifications/firebase/firebaseService.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin")
-const serviceAccount = require("./grad-proj-doctor-firebase-adminsdk-fbsvc-6831608397.json")
+const serviceAccount = require("./serviceAccountKey.json")
 
 admin.initializeApp({
     credential:admin.credential.cert(serviceAccount)
