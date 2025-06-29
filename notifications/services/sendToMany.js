2b5ab6b9889632fe7e094f303ec8b2c6a70b1209
psycho <aali571110@gmail.com>
2025-06-29 21:12:33 +0300

    test

--- notifications/services/sendToMany.js
+++ notifications/services/sendToMany.js
@@ -8,7 +8,6 @@ exports.sendToMany=async(tokens,payload)=>{
                 data:payload.data
             }
             await admin.messaging().send(message)
-
         }
     } catch (error) {
         throw error
