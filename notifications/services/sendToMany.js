cfaa467d6907a24f7863506ae2e137e3ab89ddcb
psycho <aali571110@gmail.com>
2025-06-30 04:09:34 +0300

    edit no users

--- notifications/services/sendToMany.js
+++ notifications/services/sendToMany.js
@@ -1,6 +1,7 @@
 const admin=require("../firebase/firebaseService")
 exports.sendToMany=async(tokens,payload)=>{
     try {
+        if (tokens.length === 0) return;
         for (token of tokens){
             let message={
                 notification:payload.notification,
