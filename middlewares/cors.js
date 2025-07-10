3aa8579ad95ff32529e3400ce7498d87e13b0b57
psycho <aali571110@gmail.com>
2025-07-10 15:49:07 +0300

    c

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,14 @@
-const cors=(req,res,next)=>{
+const cors = (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-    next()
-}
-exports.cors=cors
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    // التعامل مع preflight requests
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200); // إنهاء الرد فورًا
+    }
+
+    next();
+};
+
+exports.cors = cors;
